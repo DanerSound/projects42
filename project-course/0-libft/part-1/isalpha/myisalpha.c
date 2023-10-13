7baa522973a43a7a3beaf88c4b11d429b8f223bf
danersound <andre.barreto28@gmail.com>
2023-10-13 19:35:02 +0200

    base configuration for first es

---
+++ project-course/0-libft/part-1/isalpha/myisalpha.c
@@ -0,0 +1,27 @@
+#include <ctype.h>
+
+int	myisalpha(char ch)
+{
+	if ((ch >= 'a' && ch <= 'z') || (ch > 'A' && ch < 'Z'))
+	{
+		return 1024;
+	}
+	else
+	{
+		return 0;
+	}
+}
+/*
+#include <stdio.h>
+int	main() {
+	char ch ;
+	while(ch!='E')
+	{
+		printf("inserisci un carattere: ");
+		scanf("%c", &ch);
+		printf(" la funzione base ritorna %d\n",isalpha(ch));
+		printf(" la tua funzione ritorna %d\n ", myisalpha(ch));
+	}	
+	return 0;
+}*/
+
